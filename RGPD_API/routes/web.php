56a3c03e35fa8324fc236858dc72b62d23d39b0e
Ahmedio__ <ahmed__daoud@outlook.fr>
2022-11-24 10:05:27 +0100

    Redirection

--- RGPD_API/routes/web.php
+++ RGPD_API/routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('form');
 });
 
 Route::controller(\App\Http\Controllers\InformationController::class)->group(function () {
